1ab956cc6643dd171cc78db58dbd3ddd245be55c
project-term-2 <projectbisterm2@gmail.com>
2021-01-18 17:56:21 +0700

    05 arm

--- food menu/food.js
+++
@@ -1,8 +0,0 @@
-const db=firebase.firebase();
-const table=document.querySelector('#tbresult');
-db.collection('food').get().then((snapshot)=>{
-    snapshot.forEach(doc=>{
-        console.log(doc.data());
-    });
-
-});
